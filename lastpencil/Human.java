bdb347648bf2dd3027a837cdb0fc6f63386054e9
John Mirage <28307090+john-mirage@users.noreply.github.com>
2024-06-23 15:05:56 +0200

    Update Human.java

--- lastpencil/Human.java
+++ lastpencil/Human.java
@@ -1,28 +1,11 @@
 package lastpencil;
 
-import java.util.Scanner;
-
 public class Human extends Player {
-    Scanner scanner;
-
-    public Human(String name, Scanner scanner) {
+    public Human(String name) {
         super(name);
-        this.scanner = scanner;
     }
 
     public int chooseNumber(int numberOfPencils) {
-        while (true) {
-            String input = this.scanner.nextLine();
-            if (input.equals("1") || input.equals("2") || input.equals("3")) {
-                int inputNumber = Integer.parseInt(input);
-                if (numberOfPencils >= inputNumber) {
-                    return inputNumber;
-                } else {
-                    System.out.println("Too many pencils were taken");
-                }
-            } else {
-                System.out.println("Possible values: '1', '2' or '3'");
-            }
-        }
+        return UserInterface.askForNumberOfPencilsToTake(numberOfPencils);
     }
 }
